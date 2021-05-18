c008a10b23e4081afff8405d105fe6c08abdc696
Mahfooz <umahfoosahamed@gmail.com>
2021-05-18 18:21:33 +0530

    updated server

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log('request made');  // this code run in server
+    console.log(req.url, req.method); 
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch (req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    // res.write('<h1>Hello Mahfoos</h1>');
+    // res.write('<h1>Hello Mahfoos, Aganin</h1>');
+    // res.end();
+
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
+
 });
+
+
 
 server.listen(3000, 'localhost', () => {
     console.log('listening for request on port 3000')
@@ -17,3 +53,19 @@ server.listen(3000, 'localhost', () => {
 
 // Port Number - 3000   - localhost:3000
 // port number ara like door into a computer 
+
+
+// Status Codes
+// Status codes describe the type of response sent to the browser
+// 200 - OK
+// 301 - Resource moved
+// 404 - Not Found
+// 500 - internal server error
+// 100 Range - informational responses
+// 200 Range - success code
+// 300 Range - codes for redirects
+// 400 Range - user or client error codes
+// 500 Range - server error codes
+
+
+
